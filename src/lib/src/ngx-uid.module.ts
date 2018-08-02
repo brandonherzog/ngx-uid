44ea296a16ac3c22a29e77d2f9541b5c1fc2d3d7
Brandon Herzog <herzog_brandon@bah.com>
2018-08-01 22:34:23 -0400

    refactor: remove dead code and comments

--- src/lib/src/ngx-uid.module.ts
+++ src/lib/src/ngx-uid.module.ts
@@ -1,14 +1,8 @@
-import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgxUidDirective } from './ngx-uid.directive';
 import { NgxUidService, NgxUidDefaultService } from './ngx-uid.service';
-
-// export const NgxUidOptionsProvider = new InjectionToken<NgxUidOptionsProvider>("NgUidOptionsService");
-
-export function provideOptions(options?: NgxUidService): NgxUidService {
-  return options || new NgxUidDefaultService();
-}
 
 export interface NgxUidOptions {
   idFactory?: NgxUidService;
@@ -32,9 +26,7 @@ export class NgxUidModule {
       providers: [
         {
           provide: NgxUidService,
-          // useFactory: provideOptions,
           useFactory: () => (options && options.idFactory) || new NgxUidDefaultService(),
-          // useValue: options || new NgxUidDefaultService(),
         },
       ],
     };
